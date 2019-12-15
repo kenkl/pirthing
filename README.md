790faa431bfca2b4b634db139e8aa43798eb9939
kenkl <kenkl@mac.com>
2019-12-15 00:46:03 -0500

    initial pio assembly

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 COMING SOON!
 
-An ESP8266 hosted PIR sensor to send on/off commands to lights managed in the [Lights](https://github.com/kenkl/lights) app. This is based vaugely on [MotionNightlight](https://github.com/kenkl/MotionNightlight), except we don't need PWM logic to turn on/off a light; we'll just fling an on/off over to the Lights app. We will control the timings here, like MotionNightlight, to extend the on-period if additional motion is detected, for instance. 
+An ESP8266 hosted PIR sensor to send on/off commands to lights managed in the [Lights](https://github.com/kenkl/lights) app. This is based vaugely on [buttonthing](https://github.com/kenkl/buttonthing) and [MotionNightlight](https://github.com/kenkl/MotionNightlight), except we don't need PWM logic to turn on/off a light; we'll just fling an on/off over to the Lights app. We will control the timings here, like MotionNightlight, to extend the on-period if additional motion is detected, for instance. 
---
+++ src/pirthing.cpp
@@ -0,0 +1,122 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+
+// put the WIFI credentials in wificreds.h, in the same directory as pirthing.cpp (this file) with the lines:
+// const char* ssid = "WIFI SSID";
+// const char* password = "WIFI SECRET PASSWORD";
+// (uncommented, of course)
+#include "wificreds.h"
+
+const char* host = "max.kenkl.org";
+const unsigned int clienttimeout = 60000;  //ms for client timeout in the doThing* action
+#define pLED 13 //Pilot light for the PIR, in case it doesn't have its own LED
+#define pIR 5 // the PIR itself - active HIGH.
+#define led 0 // the on-board red LED - light up when calling max
+#define ledDelay 33000 // ms, of course - how long does the nightlight stay on after PIR trigger?
+unsigned long nowcount, delaycount; // tracking time for non-blocking delays
+const char* urlon = "/lights/pirthing1_on.php"; 
+const char* urloff = "/lights/pirthing1_off.php"; 
+bool iState = false; // tracking whether the light is on 
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pIR, INPUT);
+
+  Serial.begin(9600);
+
+    // We start by connecting to a WiFi network
+Serial.println();
+Serial.println();
+Serial.print("Connecting to ");
+Serial.println(ssid);
+
+/* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+   would try to act as both a client and an access-point and could cause
+   network-issues with your other WiFi-devices on your WiFi-network. */
+WiFi.mode(WIFI_STA);
+WiFi.begin(ssid, password);
+
+while (WiFi.status() != WL_CONNECTED) {
+  delay(500);
+  Serial.print(".");
+}
+
+Serial.println("");
+Serial.println("WiFi connected");
+Serial.println("IP address: ");
+Serial.println(WiFi.localIP());
+}
+
+void loop() {
+   nowcount = millis();
+
+  // if the pIR sensor has tripped, update delaycount accordingly.  
+  if(digitalRead(pIR) == HIGH) {
+    delaycount = nowcount + ledDelay ;
+    //digitalWrite(pLED, HIGH);  //optional, depending on whether the PIR already has an LED,
+  }
+  else digitalWrite(pLED, LOW);
+  
+  // now, check that delaycount has not yet been reached and turn on/off
+  // the red LEDs accordingly.
+  
+  if(millis() <= delaycount) {
+    hueON();
+  }
+  else {
+    hueOFF();
+  }
+}
+void hueON(void) {
+  if(iState == true) return; // we're already on; don't need to do anything here
+  doThing(urlon);
+  iState = true;
+}
+
+void hueOFF(void) {
+  if(iState == false) return; // we're already off; don't need to do anything here. when does this happen?
+  doThing(urloff);
+  iState = false;
+}
+void doThing(const char* url) {
+
+  digitalWrite(led, 1); // light up a pilot while we connect, send, and get a response.
+
+  Serial.println("Got to doThing...");
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  // Use WiFiClient class to create TCP connections
+WiFiClient client;
+const int httpPort = 80;
+if (!client.connect(host, httpPort)) {
+  Serial.println("connection failed");
+  return;
+}
+
+// We now create a URI for the request
+//String url = "/lights/cinema.php";
+
+Serial.print("Requesting URL: ");
+Serial.println(url);
+
+// This will send the request to the server
+client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+             "Host: " + host + "\r\n" +
+             "Connection: close\r\n\r\n");
+unsigned long timeout = millis();
+while (client.available() == 0) {
+  if (millis() - timeout > clienttimeout) {
+    Serial.println(">>> Client Timeout !");
+    client.stop();
+    return;
+  }
+}
+
+  Serial.println("closing connection");
+  Serial.println();
+
+  digitalWrite(led, 0); // turn off the pilot
+
+}
